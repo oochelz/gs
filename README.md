927159a990be16402615c2fa4057568b17bb2513
oochelz <chelsea.youmans@careerbuilder.com>
2014-09-23 16:44:08 -0400

    add in git status script and a readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Git Status
+
+This script is to call `git status` on certain Rails apps we develop.
+
+To run, put the file in the same directory as the repos, then just: `sh pbp.sh`
---
+++ gs.sh
@@ -0,0 +1,30 @@
+echo "$(tput setaf 5)js-global status:$(tput sgr0)"
+cd js-global
+git status
+
+echo "\n$(tput setaf 5)consumer-domain status:$(tput sgr0)"
+cd ../consumer-domain
+git status
+
+echo "\n$(tput setaf 5)consumer-session status:$(tput sgr0)"
+cd ../consumer-session
+git status
+
+echo "\n$(tput setaf 5)domain-values status:$(tput sgr0)"
+cd ../domain-values
+git status
+
+echo "\n$(tput setaf 5)consumer-main status:$(tput sgr0)"
+cd ../consumer-main
+git status
+
+echo "\n$(tput setaf 5)consumer-profile status:$(tput sgr0)"
+cd ../consumer-profile
+git status
+
+echo "\n$(tput setaf 5)consumer-resume status:$(tput sgr0)"
+cd ../consumer-resume
+git status
+
+cd ..
+
