825bdcb0a79af25621ce606b3ff5bf28276d1865
ausmith <aaron.goldsmith@careerbuilder.com>
2015-11-16 11:20:45 -0500

    Switch to a loop and not changing directories

--- gs.sh
+++ gs.sh
@@ -1,22 +1,14 @@
-echo "\n$(tput setaf 5)consumer-layouts status:$(tput sgr0)"
-cd consumer-layouts
-git status
+#!/usr/bin/env bash
 
-echo "\n$(tput setaf 5)consumer-domain status:$(tput sgr0)"
-cd ../consumer-domain
-git status
+DIRS="consumer-layouts
+consumer-domain
+consumer-session
+domain-values
+consumer-main"
 
-echo "\n$(tput setaf 5)consumer-session status:$(tput sgr0)"
-cd ../consumer-session
-git status
-
-echo "\n$(tput setaf 5)domain-values status:$(tput sgr0)"
-cd ../domain-values
-git status
-
-echo "\n$(tput setaf 5)consumer-main status:$(tput sgr0)"
-cd ../consumer-main
-git status
-
-cd ..
+for dir in $DIRS
+do
+  echo "$(tput setaf 5)$dir status:$(tput sgr0)"
+  git --git-dir=$dir/.git --work-tree=$dir status
+done
 
